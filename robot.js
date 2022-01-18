71278aec14c9f90bc5a766dc53f30b9ca95f62db
unknown <tloukit>
2022-01-18 20:57:14 +0100

    ajout des variables environement et transformation robot en script

--- robot.js
+++ robot.js
@@ -1,33 +1,30 @@
-const express = require('express');
-const app = express();
 const request = require('request');
 const mongoose = require("mongoose");
 
 // Modeles de donnees mongoose
 const PollutionCity = require('./models/m-PollutionCity');
 
-//URL API externe: (ici pollution pour la ville de paris)
-const url = "http://api.waqi.info/feed/paris/?token=dc1344cd36c9cafe553b59a89e93636b4ee004eb" ;//ici avec api-key
+const MONGO_USER = process.env.MONGO_USER;
+const MONGO_PWD = process.env.MONGO_PWD;
+const MONGO_CLUSTER = process.env.MONGO_CLUSTER;
 
-//mongoose.connect('mongodb+srv://isikaal10:Naturel75!@cluster0.w7hnq.mongodb.net/air_quality_db?retryWrites=true&w=majority',
-mongoose.connect('mongodb://localhost:27017/test?retryWrites=true&w=majority',
+const INSERT_EACH_5_HOURS = 18000000;
+const INSERT_EACH_5_SECONDS = 5000;
+
+mongoose.connect(`mongodb+srv://${MONGO_USER}:${MONGO_PWD}@${MONGO_CLUSTER}?retryWrites=true&w=majority`,
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
- // requetes api externe toutes les ... ms 
-const PollutionDataByCityTimer = setInterval(getPollutionDataByCity, 5000); //toutes les 5heures = 18000000
+ // requetes api externe toutes les ... en ms 
+const PollutionDataByCityTimer = setInterval(getPollutionDataByCity, INSERT_EACH_5_SECONDS);
 
 // recuperation depuis l'api des donnees de pollution pour la ville de paris
-function getPollutionDataByCity(){
 	request(url, function(error, response, body) {
 		if(response.statusCode==200){
 			console.log("statut 200 ok requete api");
 			let respValue = JSON.parse(body);
-			console.log("index Q air => " + JSON.stringify(respValue.data.iaqi.pm25["v"] ));
-			console.log("ville Q air => " + JSON.stringify(respValue.data.city.name));
-			console.log(JSON.stringify(respValue.data.time["s"]));
 		
 			// champs à insérer
 			const city = respValue.data.city.name;
@@ -48,8 +45,3 @@ function getPollutionDataByCity(){
 			pollutionCity.save();
 				}
 			});
-	}
-	
-	app.listen(8282 , function () {
-		console.log("http://localhost:8282");
-	  });
